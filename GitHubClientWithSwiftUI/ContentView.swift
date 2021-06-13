f50f1807909fab7662ece1b90b34fdcd6c618982
hidec-7 <hiedematoaho0730127@gmail.com>
2021-06-14 00:23:09 +0900

    first commit

---
+++ GitHubClientWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitHubClientWithSwiftUI
+//
+//  Created by hideto c. on 2021/06/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GitHubClientWithSwiftUI/GitHubClientWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubClientWithSwiftUIApp.swift
+//  GitHubClientWithSwiftUI
+//
+//  Created by hideto c. on 2021/06/14.
+//
+
+import SwiftUI
+
+@main
+struct GitHubClientWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
